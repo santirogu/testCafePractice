02a477c404e3e31c1e7e424fef750c586fd1caf6
SantiagoRG <srodriguezg@psl.com.co>
2020-02-07 15:30:36 -0500

    Progress.

---
+++ data/credentials.js
@@ -0,0 +1,5 @@
+export default
+{
+  'username':'srodriguezg@eroyal.net',
+  'password': 'S4nt1_2020'
+}
---
+++ tests/evidenceReview.js
@@ -0,0 +1,21 @@
+import { Selector } from 'testcafe';
+import credentials from '../data/credentials'
+
+fixture `Edit Evidence Metadata`
+    .page `https://qacommand-autom.usgoviowa.cloudapp.usgovcloudapi.net:803/`;
+
+    test('Start', async t => {
+        const evidencesList = Selector('.recent-video-section ul li')
+        await t
+            .maximizeWindow()
+            .typeText('#Username', credentials.username)
+            .typeText('#Password', credentials.password)
+            .click('.login')
+            .click('.ad-pn-32')
+            .click('.adv-search-btn')
+            .click('#jsonIdSearchDateStartedFrom input')
+            .pressKey('left left left left left')
+            .typeText('#jsonIdSearchDateStartedFrom input', '01012020')
+            .click('.frm-controls .button-primary')
+            .click(evidencesList.nth(0))
+    });
