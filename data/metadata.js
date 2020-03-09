080731aea924007c279ae4d4ad1b2a20ec459a2d
SantiagoRG <srodriguezg@psl.com.co>
2020-03-09 15:33:46 -0500

    Se agregó la metadata cómo un elemento extra en un json.

---
+++ data/metadata.js
@@ -0,0 +1,17 @@
+export default
+{ 
+    'officerName' : '1111',
+    'partner'     : 'Thor',
+    'unit'        : 'H1-01',
+    'event'       : 'Default',
+    'ticketOne'   : 'Ticket-01',
+    'ticketTwo'   : 'Ticket-02',
+    'dispatchOne' : 'Dispatch-01',
+    'dispatchTwo' : 'Dispatch-02',
+    'notes'       : 'Automation',
+    'retained'    : 'Yes',
+    'latitude'    : '6.179402',
+    'longitude'   : '-75.595254',
+    'location'    : 'La Esmeralda, Itagüi, Antioquia',
+    'triggeredBy' : 'Other'
+}
--- tests/evidenceReview.js
+++ tests/evidenceReview.js
@@ -1,8 +1,9 @@
-import credentials from '../data/credentials';
-import LoginPage from '../pages/login/sso/loginPage';
-import AdminDashboardPage from '../pages/admin_dashboard/adminDashboardPage';
+import credentials          from '../data/credentials';
+import metadata             from '../data/metadata';
+import LoginPage            from '../pages/login/sso/loginPage';
+import AdminDashboardPage   from '../pages/admin_dashboard/adminDashboardPage';
 import OfficerDashboardPage from '../pages/officer_dashboard/officerDashboardPage';
-import EvidenceReviewPage from '../pages/evidenceReview/evidenceReviewPage';
+import EvidenceReviewPage   from '../pages/evidenceReview/evidenceReviewPage';
 
 fixture `Edit Evidence Metadata`
     .beforeEach(async (t) => {
@@ -11,22 +12,6 @@ fixture `Edit Evidence Metadata`
     });
 
     test('Edit Basic Information on metadata pop-up', async t => {
-        const metadata = { 
-            officerName : '1111',
-            partner     : 'Thor',
-            unit        : 'H1-01',
-            event       : 'Default',
-            ticketOne   : 'Ticket-01',
-            ticketTwo   : 'Ticket-02',
-            dispatchOne : 'Dispatch-01',
-            dispatchTwo : 'Dispatch-02',
-            notes       : 'Automation',
-            retained    : 'Yes',
-            latitude    : '6.179402',
-            longitude   : '-75.595254',
-            location    : 'La Esmeralda, Itagüi, Antioquia',
-            triggeredBy : 'Other'
-        };
         await AdminDashboardPage.gotoOfficerDashboard();
         await OfficerDashboardPage.search({ dateStartedFrom : '01012020' });
         await OfficerDashboardPage.openEvidence(0);
